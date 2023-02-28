1182179947bc39a99678876ef4533f78344cd8bc
bim22614 <64481896+bim22614@users.noreply.github.com>
2023-02-28 12:24:35 +0200

    Update db.php

--- db.php
+++ db.php
@@ -32,7 +32,7 @@ mysqli_query($link, $sql);
 
 $old_path = getcwd();
 chdir('/home/ubuntu');
-$output = shell_exec(`curl -X POST http://100.25.153.111:8080/job/mail/build --user bim:117799b22b54caf2127a188736a0a50d6c --data-urlencode json='{'parameter': [{"name":"mail", "value":"$var_mail"}, {"name":"ip", "value":"$var_ip"}, {"name":"mask", "value":"$var_mask"}]}'`);
+$output = shell_exec(`curl -X POST http://52.204.141.215:8080/job/mail/build --user bim:117799b22b54caf2127a188736a0a50d6c --data-urlencode json='{'parameter': [{"name":"mail", "value":"$var_mail"}, {"name":"ip", "value":"$var_ip"}, {"name":"mask", "value":"$var_mask"}]}'`);
 
 chdir($old_path);
 echo "<pre>$output</pre>";
